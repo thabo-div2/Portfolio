e2c95889ea825e174cbb25b6651111e1213c9b37
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-07-19 08:30:49 +0200

    Update

--- Scripts/Projects.js
+++ Scripts/Projects.js
@@ -31,75 +31,74 @@
 // // }
 
 let projects = [
-  {
-    imgURL: "./Images/Screenshot_2021-04-30 Testimonials.png",
-    imgALT: "Testimonial",
-    title: "Testimonial Exercise",
-    techStack: "HTML/CSS",
-    description: "An exercise showing how I used owl-carousel",
-    githubURL: "https://github.com/thabo-div2/Testimonials_Sliders",
-    liveProjectURL: "https://optimistic-bardeen-a13c08.netlify.app/",
-  },
-  {
-    imgURL: "./Images/Screenshot_2021-05-12 Project Cards.png",
-    imgALT: "Project Cards",
-    title: "Project Cards Exercise",
-    techStack: "HTML/CSS",
-    description: "An exercise showing and styling my previous projects",
-    githubURL: "https://github.com/thabo-div2/Project_Cards",
-    liveProjectURL: "https://confident-aryabhata-78e10b.netlify.app",
-  },
-  {
-    imgURL: "./Images/Screenshot_2021-04-30 Thabo's Contact Form.png",
-    imgALT: "Contact",
-    title: "Contact Form Exercise",
-    techStack: "",
-    description:
-      "An exercise that shows a simple contact page. Styled with Neumorphism",
-    githubURL: "https://github.com/thabo-div2/Contact_Form",
-    liveProjectURL: "https://jolly-shaw-42b983.netlify.app/",
-  },
-  {
-    imgURL: "./Images/Screenshot_2021-04-30 My Timeline.png",
-    imgALT: "Timeline",
-    title: "Timeline Exercise",
-    techStack: "HTML/CSS",
-    description: "An exercise that shows a sample Timeline",
-    githubURL: "https://github.com/thabo-div2/My_Timeline",
-    liveProjectURL: "https://mystifying-hermann-44afd1.netlify.app/",
-  },
-  {
-    imgURL: "./Images/Screenshot_2021-05-12 Sneaker Store(1).png",
-    imgALT: "Sneaker",
-    title: "Sneaker Website Exercise",
-    techStack: "HTML/CSS",
-    description: "A basic Sneaker Website",
-    githubURL: "https://github.com/thabo-div2/Project_Cards",
-    liveProjectURL: "https://hungry-hodgkin-a8c68d.netlify.app",
-  },
-  {
-    imgURL: "./Images/Screenshot_2021-05-12 python_tkinter_exercise.png",
-    imgALT: "Tkinter",
-    title: "Add Two Numbers",
-    techStack: "Python",
-    description: "A simple tkinter programme that adds two numbers",
-    githubURL:
-      "https://github.com/thabo-div2/python_tkinter_exercise/blob/main/Exercise.py",
-    liveProjectURL: "https://replit.com/@ThaSet/pythontkinterexercise#main.py",
-  },
-  {
-    imgURL: "./Images/Screenshot_2021-05-12 BMI_Exercise-1.png",
-    imgALT: "BMI",
-    title: "Contact Form Exercise",
-    techStack: "Python",
-    description: "A tkinter exercise that calculates a persons BMI",
-    githubURL: "https://github.com/thabo-div2/BMI_Exercise/blob/main/BMI.py",
-    liveProjectURL: "https://replit.com/@ThaSet/BMIExercise-1#main.py",
-  },
+	{
+		imgURL: "./Images/Screenshot_2021-04-30 Testimonials.png",
+		imgALT: "Testimonial",
+		title: "Testimonial Exercise",
+		techStack: "HTML/CSS",
+		description: "An exercise showing how I used owl-carousel",
+		githubURL: "https://github.com/thabo-div2/Testimonials_Sliders",
+		liveProjectURL: "https://optimistic-bardeen-a13c08.netlify.app/",
+	},
+	{
+		imgURL: "./Images/Screenshot_2021-05-12 Project Cards.png",
+		imgALT: "Project Cards",
+		title: "Project Cards Exercise",
+		techStack: "HTML/CSS",
+		description: "An exercise showing and styling my previous projects",
+		githubURL: "https://github.com/thabo-div2/Project_Cards",
+		liveProjectURL: "https://confident-aryabhata-78e10b.netlify.app",
+	},
+	{
+		imgURL: "./Images/Screenshot_2021-04-30 Thabo's Contact Form.png",
+		imgALT: "Contact",
+		title: "Contact Form Exercise",
+		techStack: "HTML/CSS",
+		description: "An exercise that shows a simple contact page.",
+		githubURL: "https://github.com/thabo-div2/Contact_Form",
+		liveProjectURL: "https://jolly-shaw-42b983.netlify.app/",
+	},
+	{
+		imgURL: "./Images/Screenshot_2021-04-30 My Timeline.png",
+		imgALT: "Timeline",
+		title: "Timeline Exercise",
+		techStack: "HTML/CSS",
+		description: "An exercise that shows a sample Timeline",
+		githubURL: "https://github.com/thabo-div2/My_Timeline",
+		liveProjectURL: "https://mystifying-hermann-44afd1.netlify.app/",
+	},
+	{
+		imgURL: "./Images/Screenshot_2021-05-12 Sneaker Store(1).png",
+		imgALT: "Sneaker",
+		title: "Sneaker Website Exercise",
+		techStack: "HTML/CSS",
+		description: "A basic Sneaker Website",
+		githubURL: "https://github.com/thabo-div2/Project_Cards",
+		liveProjectURL: "https://hungry-hodgkin-a8c68d.netlify.app",
+	},
+	{
+		imgURL: "./Images/Screenshot_2021-05-12 python_tkinter_exercise.png",
+		imgALT: "Tkinter",
+		title: "Add Two Numbers",
+		techStack: "Python",
+		description: "A simple tkinter programme that adds two numbers",
+		githubURL:
+			"https://github.com/thabo-div2/python_tkinter_exercise/blob/main/Exercise.py",
+		liveProjectURL: "https://replit.com/@ThaSet/pythontkinterexercise#main.py",
+	},
+	{
+		imgURL: "./Images/Screenshot_2021-05-12 BMI_Exercise-1.png",
+		imgALT: "BMI",
+		title: "Contact Form Exercise",
+		techStack: "Python",
+		description: "A tkinter exercise that calculates a persons BMI",
+		githubURL: "https://github.com/thabo-div2/BMI_Exercise/blob/main/BMI.py",
+		liveProjectURL: "https://replit.com/@ThaSet/BMIExercise-1#main.py",
+	},
 ];
 
 function createCard(card) {
-  let createdCard = `
+	let createdCard = `
   <div class="cards-container fade" techStack=${card.techStack}>
     <div class="cards-items">
       <img class="project-img" src="${card.imgURL}" alt="${card.imgALT}">
@@ -111,7 +110,7 @@ function createCard(card) {
           <a href="${card.githubURL}">
             <button>Github</button>
           </a>
-          <a href="${card.liveProjectURL}>
+          <a href="${card.liveProjectURL}">
             <button>Live Link</button>
           </a>
         </div>
@@ -119,15 +118,15 @@ function createCard(card) {
     </div>
   </div>
   `;
-  return createdCard;
+	return createdCard;
 }
 
 function renderCards() {
-  let projectContainer = document.querySelector(".project-cards");
-  for (project of projects) {
-    let card = createCard(project);
-    projectContainer.innerHTML += card;
-  }
+	let projectContainer = document.querySelector(".project-cards");
+	for (project of projects) {
+		let card = createCard(project);
+		projectContainer.innerHTML += card;
+	}
 }
 
 renderCards();
